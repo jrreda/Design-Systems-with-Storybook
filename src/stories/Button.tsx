68249eadff261e8f458002c0589d274aa65eeecc
Reda <mahmoudreda457@gmail.com>
2026-07-02 10:20:07 +0300

    Button variants

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,3 +1,14 @@
-export const Button = (props: any) => {
-  return <button {...props} />;
+import type { ComponentProps } from "react";
+
+import './button.css'
+import clsx from "clsx";
+
+export type ButtonProps = ComponentProps<"button"> & {
+  variant: "primary" | "secondary" | "destructive";
+};
+
+export const Button = ({ variant, ...props }: ButtonProps) => {
+  const className = clsx("button", variant);
+
+  return <button className={className} {...props} />;
 };
